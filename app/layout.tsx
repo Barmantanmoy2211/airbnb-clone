921e4a2ee33391e49143ed442f52cef5857f965c
Tanmoy Barman <barmantanmoy59@gmail.com>
2023-04-03 21:37:57 +0530

    Auth ui init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 import './globals.css'
+import ClientOnly from "./components/ClientOnly";
 
 const font = Nunito({
   subsets : ['latin'],
@@ -16,7 +18,10 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body className={font.className}>
+        <ClientOnly>
+          <Modal title="Hello World" isOpen/>
         <Navbar />
+        </ClientOnly>
         {children}
         </body>
     </html>
