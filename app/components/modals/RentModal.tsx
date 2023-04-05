49a92296d218963b0d2cd873c10551bceb85b7a3
Tanmoy Barman <barmantanmoy59@gmail.com>
2023-04-05 13:58:07 +0530

    Counter components Doen

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from 'next/dynamic'
+import Counter from "../inputs/Counter";
 
 
 enum STEPS {
@@ -145,26 +146,26 @@ const RentModal = () =>{
               title="Share some basics about your place"
               subtitle="What amenitis do you have?"
             />
-            {/* <Counter 
+            <Counter 
               onChange={(value) => setCustomValue('guestCount', value)}
               value={guestCount}
               title="Guests" 
               subtitle="How many guests do you allow?"
-            /> */}
+            />
             <hr />
-            {/* <Counter 
+            <Counter 
               onChange={(value) => setCustomValue('roomCount', value)}
               value={roomCount}
               title="Rooms" 
               subtitle="How many rooms do you have?"
-            /> */}
+            />
             <hr />
-            {/* <Counter 
+            <Counter 
               onChange={(value) => setCustomValue('bathroomCount', value)}
               value={bathroomCount}
               title="Bathrooms" 
               subtitle="How many bathrooms do you have?"
-            /> */}
+            />
           </div>
         )
       }
