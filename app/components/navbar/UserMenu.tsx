d32719aa5c3ccc409186e4feccb71e6b73c7ce85
Tanmoy Barman <barmantanmoy59@gmail.com>
2023-04-05 13:11:23 +0530

    Category Selection

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,14 +1,17 @@
 'use client';
 
+import { useCallback, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
-import Avatar from "../Avatar";
-import { useCallback , useState } from "react";
-import MenuItem from "./MenuItem";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 import useLoginModal from "@/app/hooks/useLoginModal";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
-import { signOut} from "next-auth/react"
+import useRentModal from "@/app/hooks/useRentModal";
 import { SafeUser } from "@/app/types";
+
+import MenuItem from "./MenuItem";
+import Avatar from "../Avatar";
 
 interface UserMenuProps {
   currentUser?: SafeUser | null
@@ -17,93 +20,107 @@ interface UserMenuProps {
 const UserMenu: React.FC<UserMenuProps> = ({
   currentUser
 }) => {
-  const registerModal = useRegisterModal();
+  const router = useRouter();
+
   const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
+  const rentModal = useRentModal();
+
   const [isOpen, setIsOpen] = useState(false);
 
-  const toogleOpen = useCallback(()=>{
+  const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
   }, []);
 
-  return (
+  const onRent = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    rentModal.onOpen();
+  }, [loginModal, rentModal, currentUser]);
+
+  return ( 
     <div className="relative">
-        <div className="flex flex-row item-center gap-3">
-            <div onClick={() =>{}}
-            className="
-            hidden 
+      <div className="flex flex-row items-center gap-3">
+        <div 
+          onClick={onRent}
+          className="
+            hidden
             md:block
-            text-sm
-            font-semibold
-            py-3
-            px-4
-            rounded-full
-            hover:bg-neutral-100
-            transition
+            text-sm 
+            font-semibold 
+            py-3 
+            px-4 
+            rounded-full 
+            hover:bg-neutral-100 
+            transition 
             cursor-pointer
-            "
-            >
-                Airbnb your home
-            </div>
-            <div
-            onClick={toogleOpen}
-            className="
-            p-4
-            md:py-1
-            md:px-2
-            border-[1px]
-            border-neutral-200
-            flex
-            flex-row
-            items-center
-            gap-3
-            rounded-full
-            cursor-pointer
-            hover:shadow-md
-            transition
-            "
-            >
-              <AiOutlineMenu />
-              <div className="hidden md:block">
-                <Avatar src={currentUser?.image} />
-              </div>
-            </div>
-        </div>
-      {isOpen && (
-        <div
-        className="
-        absolute
-        rounded-xl
-        shawdow-md
-        w-[40vw]
-        md:w-3/4bg-white
-        overflow-hidden
-        right-0
-        top-12
-        text-sm
-        "
+          "
         >
-          <div className="flex flex-col courser-pointer">
-          {currentUser ? (
+          Airbnb your home
+        </div>
+        <div 
+        onClick={toggleOpen}
+        className="
+          p-4
+          md:py-1
+          md:px-2
+          border-[1px] 
+          border-neutral-200 
+          flex 
+          flex-row 
+          items-center 
+          gap-3 
+          rounded-full 
+          cursor-pointer 
+          hover:shadow-md 
+          transition
+          "
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar src={currentUser?.image} />
+          </div>
+        </div>
+      </div>
+      {isOpen && (
+        <div 
+          className="
+            absolute 
+            rounded-xl 
+            shadow-md
+            w-[40vw]
+            md:w-3/4 
+            bg-white 
+            overflow-hidden 
+            right-0 
+            top-12 
+            text-sm
+          "
+        >
+          <div className="flex flex-col cursor-pointer">
+            {currentUser ? (
               <>
                 <MenuItem 
                   label="My trips" 
-                  onClick={() => {}}
+                  onClick={() => router.push('/trips')}
                 />
                 <MenuItem 
                   label="My favorites" 
-                  onClick={() => {}}
+                  onClick={() => router.push('/favorites')}
                 />
                 <MenuItem 
                   label="My reservations" 
-                  onClick={() => {}}
+                  onClick={() => router.push('/reservations')}
                 />
                 <MenuItem 
                   label="My properties" 
-                  onClick={() => {}}
+                  onClick={() => router.push('/properties')}
                 />
                 <MenuItem 
                   label="Airbnb your home" 
-                  onClick={()=>{}}
+                  onClick={rentModal.onOpen}
                 />
                 <hr />
                 <MenuItem 
@@ -127,7 +144,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
         </div>
       )}
     </div>
-  )
+   );
 }
-
-export default UserMenu
+ 
+export default UserMenu;
